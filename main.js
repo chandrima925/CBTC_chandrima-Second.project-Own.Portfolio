8cd1d5ad020ad3b60907031dbd20437f00d7fb52
chandrima925 <chandrimasengupta925@gmail.com>
2024-03-24 00:12:46 +0530

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
- 
+let aboutSection = document.querySelector("#about");
+window.onscroll = function(){
+    menubar();
+}
+
+function menubar(){
+    if (document.documentElement.scrollTop>=about.offsetTop){
+        document.querySelector(".menu-bar").classList.add(".sticky");
+    }
+    else{
+        document.querySelector(".menu-bar").classList.remove(".sticky");
+    }
+}
